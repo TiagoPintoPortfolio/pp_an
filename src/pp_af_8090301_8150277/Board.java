9806cb4a9fd4126c94b7fe66fe635ad98d0583b1
Tiago Pinto <8150277@estg.ipp.pt>
2018-06-05 18:53:39 +0100

    Board

---
+++ src/pp_af_8090301_8150277/Board.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pp_af_8090301_8150277;
+
+import game.abstractClasses.BoardAbstract;
+import game.abstractClasses.BrickAbstract;
+import game.abstractClasses.LevelAbstract;
+import game.colections.ListContract;
+import game.interfaces.ClassificationContract;
+import game.interfaces.PlayerContract;
+
+/**
+ *
+ * @author Tiago Pinto
+ */
+public class Board extends BoardAbstract {
+
+    @Override
+    public PlayerContract getPlayer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setPlayer(PlayerContract pc) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ListContract getLevels() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setLevels(ListContract lc) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addLevel(LevelAbstract la) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ClassificationContract getClassification() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setClassification(ClassificationContract cc) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public BrickAbstract[] createBrickArray(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
--- src/pp_af_8090301_8150277/PP_AF_8090301_8150277.java
+++ src/pp_af_8090301_8150277/PP_AF_8090301_8150277.java
@@ -5,6 +5,10 @@
  */
 package pp_af_8090301_8150277;
 
+import game.exceptions.NoBricksException;
+import game.interfaces.ClassificationContract;
+import game.main.Game;
+
 /**
  *
  * @author Tiago Pinto
@@ -14,13 +18,20 @@ public class PP_AF_8090301_8150277 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoBricksException, InterruptedException {
         //TESTE
         System.out.println("OLA GIT");
         
         System.out.println("HELLO GIT TIAGO");
         
         System.out.println("GIT FINAL");
+        
+        /*Game game = new Game(b);
+        game.initUI();
+        game.waitUntilClosed();
+        ClassificationContract classification = game.getLastClassification();
+        System.out.println(classification.toString());
+        */
         
         
     }
